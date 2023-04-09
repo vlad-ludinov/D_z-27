19817d2bf6833f7096c3c1a1e1f44285fb7bc367
vlad <vladislavludinov3006@gmail.com>
2023-04-09 13:14:45 +0300

    main menu added

---
+++ controller.py
@@ -0,0 +1,23 @@
+import view
+
+
+def start_pb():
+    while True:
+        choice = view.main_menu()
+        match choice:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+            case 6:
+                pass
+            case 7:
+                pass
+            case 8:
+                pass
